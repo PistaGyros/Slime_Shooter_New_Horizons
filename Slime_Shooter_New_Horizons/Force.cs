01d50f95d3dbad12190e316fd2ca1a1028086e39
PistaGyros <tomko.jurcik@gmail.com>
2025-03-16 14:31:41 +0100

    Force class
    More scallable and independent physics

--- Slime_Shooter_New_Horizons/Force.cs
+++ Slime_Shooter_New_Horizons/Force.cs
@@ -1,20 +1,45 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 
 
 namespace Slime_Shooter_New_Horizons;
 
 public class Force
 {
+    public bool fly;
+    public float time;
     public Vector2 initVelocity;
     public Vector2 initPos;
+    public int initQuadrant;
+    public float power = 200;
+    public float gravityAcceleration;
     
     public Force()
     {
         
     }
 
-    public void Throw(Rectangle destinationRectangle, Vector2 force)
+    public void Throw(Rectangle destinationRectangle)
     {
+        initVelocity = new Vector2(MathF.Cos(40 * MathF.PI / 180), MathF.Sin(215 * MathF.PI / 180)) * power;
+        initPos = new Vector2(destinationRectangle.X, destinationRectangle.Y);
+        fly = true;
+    }
+    
+    public Rectangle Fly(GameTime gameTime, Rectangle destinationRectangle)
+    {
+        time += (float)gameTime.ElapsedGameTime.TotalSeconds * 3;
+
+        float newX = KinematicEquation(5f, initVelocity.X, initPos.X, time);
+        float newY = KinematicEquation(gravityAcceleration, initVelocity.Y, initPos.Y, time);
         
+        destinationRectangle.X = (int)newX;
+        destinationRectangle.Y = (int)newY;
+        return new Rectangle(destinationRectangle.X, destinationRectangle.Y, destinationRectangle.Width, destinationRectangle.Height);
+    }
+    
+    private float KinematicEquation(float acceleration, float velocity, float position, float time)
+    {
+        return 0.5f * acceleration * time * time + velocity * time + position;
     }
 }
--- Slime_Shooter_New_Horizons/Slime.cs
+++ Slime_Shooter_New_Horizons/Slime.cs
@@ -6,25 +6,36 @@ namespace Slime_Shooter_New_Horizons;
 
 public class Slime : Animator
 {
-    private bool fly;
-    private float time;
-    private Vector2 initVelocity;
-    private Vector2 initPos;
-    private int initQuadrant;
-    private float power = 200;
-    private float gravityAcceleration;
-    
     public Slime(Texture2D texture, Rectangle destinationRectangle, Rectangle sourceRectangle, Vector2 colliderSize,
         float scaleMultiplier, int numFrames, int numCollums, Vector2 size, int quadrantSpawned) :
         base(texture, destinationRectangle, sourceRectangle, colliderSize, scaleMultiplier, numFrames, numCollums, size)
     {
-        initVelocity = new Vector2(MathF.Cos(40 * MathF.PI / 180), MathF.Sin(215 * MathF.PI / 180)) * power;
-        initPos = new Vector2(destinationRectangle.X, destinationRectangle.Y);
+        Throw(destinationRectangle);
         initQuadrant = quadrantSpawned;
         initVelocity *= DecideWhatinitQuadrant(quadrantSpawned);
-        fly = true;
     }
 
+    
+
+    public override void Update(GameTime gameTime)
+    {
+        UpdateAnimator(gameTime);
+        if (fly)
+        {
+            destinationRectangle = Fly(gameTime, destinationRectangle);
+        }
+
+        if (initQuadrant == 1 | initQuadrant == 3 && destinationRectangle.Y >= initPos.Y + 46)
+        {
+            fly = false;
+        }
+        else if (initQuadrant == 4 && destinationRectangle.Y >= initPos.Y + 146)
+            fly = false;
+        else if (initQuadrant == 2 && destinationRectangle.Y <= initPos.Y - 146)
+            fly = false;
+    }
+    
+    
     private Vector2 DecideWhatinitQuadrant(int quadrantSpawned)
     {
         Vector2 quadrantVec = Vector2.Zero;
@@ -50,39 +61,5 @@ public class Slime : Animator
         }
 
         return quadrantVec;
-    }
-
-    public override void Update(GameTime gameTime)
-    {
-        UpdateAnimator(gameTime);
-        if (fly)
-        {
-            Fly(gameTime);
-        }
-
-        if (initQuadrant == 1 | initQuadrant == 3 && destinationRectangle.Y >= initPos.Y + 46)
-        {
-            fly = false;
-        }
-        else if (initQuadrant == 4 && destinationRectangle.Y >= initPos.Y + 146)
-            fly = false;
-        else if (initQuadrant == 2 && destinationRectangle.Y <= initPos.Y - 146)
-            fly = false;
-    }
-    
-    private float KinematicEquation(float acceleration, float velocity, float position, float time)
-    {
-        return 0.5f * acceleration * time * time + velocity * time + position;
-    }
-
-    private void Fly(GameTime gameTime)
-    {
-        time += (float)gameTime.ElapsedGameTime.TotalSeconds * 3;
-
-        float newX = KinematicEquation(5f, initVelocity.X, initPos.X, time);
-        float newY = KinematicEquation(gravityAcceleration, initVelocity.Y, initPos.Y, time);
-        
-        destinationRectangle.X = (int)newX;
-        destinationRectangle.Y = (int)newY;
     }
 }
